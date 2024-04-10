846c7611c8ad3b0551ebd772c972d411a1b5fa95
d33bs <dave.bunten@cuanschutz.edu>
2024-04-10 16:45:46 -0600

    use variable slice to limit svc account chars

--- {{ cookiecutter.project_name }}/terraform/operations/accounts.tf
+++ {{ cookiecutter.project_name }}/terraform/operations/accounts.tf
@@ -1,7 +1,10 @@
 # tf account creation and related work
 # Create a new service account
 resource "google_service_account" "service_account" {
-  account_id = "${var.initiative_label}-svc-account"
+  # note: template may have truncated the project name due to character limits
+  # for Google service accounts. See the following for more information:
+  # https://cloud.google.com/iam/docs/service-accounts-create#creating
+  account_id = "{{ cookiecutter.project_name[:21] }}-svc-acct"
 }
 
 #Create a service-account key for the associated service account
